cade379ba07a42efdc5d0d0d17e36dbf9593b3c8
Eric Marcinowski <ericmarcinowski@gmail.com>
2015-04-20 15:29:15 -0400

    Updated project type and .gitignore

--- Source/ComponentAssignment5/ComponentAssignment5.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "ComponentAssignment5.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ComponentAssignment5, "ComponentAssignment5" );
- 
